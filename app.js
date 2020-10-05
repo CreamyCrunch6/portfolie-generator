aad4e3cfa1072b193cd55e3ee7f94cb6088d6e0f
Joshua Gerner <joshuagerner@Joshuas-MacBook-Pro-4.local>
2020-10-04 19:51:35 -0600

    inquirer is working

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+const inquirer = require('inquirer');
+/*
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name, github);
 
-const [name , github] = profileDataArgs;
-
-fs.writeFile('./index.html', generatePage("Josh" , "CreamyCrunch6"), err => {
-  if (err) throw new Error(err);
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
+*/
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
