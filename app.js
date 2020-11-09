3c1275c86d42e624dd8f40fbe995ed77cf7ad0b7
Joshua Gerner <joshuagerner@Joshuas-MacBook-Pro-4.local>
2020-11-09 15:46:17 -0700

    jk only done with 2.6

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//const fs = require('fs');
+const fs = require('fs');
 //const generatePage = require('./src/page-template.js');
 //
 //const profileDataArgs = process.argv.slice(2);
@@ -39,5 +39,8 @@ const generatePage = (name, github) => {
   `;
 };
 
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+})
+
+console.log('Portfolio complete! Check out index.html to see the output!')
